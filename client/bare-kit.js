039fd786af62ac1417805752e044e432be09e935
Eduard Castellano <educastellano08@gmail.com>
2025-09-01 14:54:24 +0200

    Add cross spawn

---
+++ client/bare-kit.js
@@ -0,0 +1,8 @@
+exports.spawn = function spawn(filename, source, args = []) {
+  const { Worklet } = require('react-native-bare-kit')
+
+  const worklet = new Worklet()
+  await worklet.start(filename, source, args)
+
+  return worklet
+}
---
+++ client/pear.js
@@ -0,0 +1,13 @@
+exports.spawn = function spawn(filename, _, args = []) {
+  args = Array.isArray(_) ? _ : args
+
+  filename = filename.replace(/^[\\|/]/, '')
+
+  const link = Pear.key
+    ? `${Pear.config.applink}/${filename}`
+    : `${Pear.config.dir}${filename}`
+
+  const ipc = Pear.worker.run(link, args)
+
+  return { IPC: ipc }
+}
