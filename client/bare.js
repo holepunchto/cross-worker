2bb141fb2e59040115572bd5c57823e1d4db8d3e
Marco <maidh91@gmail.com>
2025-09-05 20:21:52 +1000

    support-bare

---
+++ client/bare.js
@@ -0,0 +1,15 @@
+const SubProcess = require('bare-subprocess')
+
+exports.spawn = async function spawn(filename, _, args = []) {
+  const RUNTIME = Bare.argv[0]
+
+  args = Array.isArray(_) ? _ : args
+
+  filename = filename.replace(/^[\\|/]/, '')
+  
+  const child = SubProcess.spawn(RUNTIME, [filename, ...args], {
+    stdio: ['inherit', 'inherit', 'inherit', 'pipe']
+  })
+
+  return { IPC: child.stdio[3] }
+}
--- client/index.js
+++ client/index.js
@@ -1,1 +1,9 @@
-module.exports = require('#client')
+const { isBareKit, isBare, isPear } = require('which-runtime')
+
+if (isPear) {
+  module.exports = require('./pear')
+} else if (isBare) {
+  module.exports = require('./bare')
+} else if (isBareKit) {
+  module.exports = require('./bare-kit')
+}
