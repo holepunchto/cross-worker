b3f67ae5cb108a81b74a0b75126a95ac441d9d91
Eduard Castellano <educastellano08@gmail.com>
2026-01-07 13:42:09 +0100

    Remove spawn (#9)

--- client/bare-kit.js
+++
@@ -1,8 +0,0 @@
-exports.spawn = async function spawn(filename, source, args = []) {
-  const { Worklet } = require('react-native-bare-kit')
-
-  const worklet = new Worklet()
-  await worklet.start(filename, source, args)
-
-  return worklet
-}
