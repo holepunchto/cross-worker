1e220a5878198206846e49f706bf22c22e2c1ea2
rafapaezbas <rafaelpaezbastida@gmail.com>
2026-02-16 13:47:57 +0100

    corrected isBare check

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const { isBareKit, isPear, isBare } = require('which-runtime')
 
 if (isPear) {
   module.exports = require('./lib/pear')
-} else if (isBare) {
-  module.exports = require('./lib/bare')
+  // Temporary: will be removed after exposing in bare-kit 1.15.0
 } else if (isBareKit) {
   module.exports = require('./lib/bare-kit')
+} else if (isBare) {
+  module.exports = require('./lib/bare')
 }
