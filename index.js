f01c9d58192c8b31a9ca57554dc62ba09af02b0b
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2026-02-16 17:30:56 +0100

    add bare-sidecar compatibility (#10)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { isBareKit, isPear } = require('which-runtime')
+const { isBareKit, isPear, isBare } = require('which-runtime')
 
 if (isPear) {
   module.exports = require('./lib/pear')
+  // Temporary: will be removed after exposing in bare-kit 1.15.0
 } else if (isBareKit) {
   module.exports = require('./lib/bare-kit')
+} else if (isBare) {
+  module.exports = require('./lib/bare')
 }
